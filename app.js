c0c2b6e50617481c7808f270e7ae18f70b9c632f
Prabhat <singhprabhatp.s.224@gamil.com>
2021-10-22 00:57:23 +0530

    input processing output done still needs some styling

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var inputArea = document.querySelector("textarea")
+var outputArea = document.querySelector("#output")
+
+var serverUrl = "https://api.funtranslations.com/translate/mandalorian.json"
+
+function getTranslatedText(text) {
+    return `${serverUrl}?text=${text}`
+}
 
 function Eventhandler() {
     var inputData = inputArea.value
-    console.log(inputData)
+
+    fetch(getTranslatedText(inputData))
+        .then(res => res.json())
+        .then(json => {
+            var translated = json.contents.translated
+            outputArea.innerText = translated
+            //console.log(json)
+        })
+        .catch(err => console.log(err))
+
 }
 
 btnTranslate.addEventListener("click", Eventhandler)
